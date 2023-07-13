83652d7eabe70188190f86c184c054622b1d5983
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-13 23:04:02 +0300

    First commit of C language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - A C programe that prints a line to the standard error
+ * main - 
  *
  * Description: print a quote using write function
  * write (int fd.const void *buf,size_t count)
@@ -14,5 +13,5 @@ int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, quo, 59);
-return (0);
+return (1);
 }
