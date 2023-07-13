e6f746c74a223f0fcd18151220ff92cdf8cf3e80
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-13 23:01:09 +0300

    First commit of C language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - A C programe that prints a line to the standard error
  *
  * Description: print a quote using write function
  * write (int fd.const void *buf,size_t count)
