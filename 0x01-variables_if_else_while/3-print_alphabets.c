47245a1a2501624edf2faf5e6319f841c3eaa56a
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-14 22:01:41 +0300

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase and then in uppercase
+ *
+ * Return:(0)
+ *
+*/
+
+int main (void)
+{
+int lett_lower, lett_upper;
+for (lett_lower= 'a'; lett_lower <= 'z'; lett_lower++)
+put char("lerr_lower\n");
+for (lett_upper= 'A'; lett_upper <= 'Z'; lett_upper++)
+put char ("lett_upper");
+put char ("\n")
+return (0);
+}
