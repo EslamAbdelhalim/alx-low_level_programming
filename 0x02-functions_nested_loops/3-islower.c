e01fce4cf9cb524156fdf121e8a67d49863ad7c6
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-18 23:49:34 +0300

    Function and nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- * int _islower(int c) - function that checks for lowercase character.
- *
- * Returns 1 if c is lowercase
- * Returns 0 otherwise
- *
+ * _islower - function to check for lowercase character
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
 */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,15 @@
 #include "main.h"
 /**
- * int _isalpha(int c) - unction that checks for alphabetic character. 
- *
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
  * @c: is the int that will use for the argument of the function
- *
+ * Return: 0
 */
 int _isalpha(int c)
 {
-  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-				 {
-				   return (1);
-				 }
-  else
-    {
-      return (0);
-    }
-
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	return (0);
 }
