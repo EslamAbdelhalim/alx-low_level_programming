b270b2e621e4f6bb4a49b26c49606cae33c812e0
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-18 22:51:46 +0300

    Function and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
- */
+*/
 void print_alphabet_x10(void)
 {
 	char c, i;
@@ -12,8 +12,8 @@ void print_alphabet_x10(void)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(c);
+		  _putchar(c);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
