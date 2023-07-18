f4a28c2c5d03431e58dc7ba6e1b45fcd3ce8c25a
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-18 23:07:22 +0300

    Function and nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "maim.h"
+/**
+ *int _isalpha(int c) - unction that checks for alphabetic character. 
+ *
+ * Returns 1 if c is a letter, lowercase or uppercase
+ *  Returns 0 otherwise
+ *
+*/
+int _isalpha(int c)
+{
+  if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+				 {
+				   return (1);
+				 }
+  else
+    {
+      return (0);
+    }
+
+}
