abdef24b957b9e8e76d66da56a25800fc0994c03
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-20 14:29:10 +0300

    MOre function more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _isdigit(int c) - unction that checks for a digit (0 through 9).
+ * _isdigit - function that checks for a digit (0 through 9).
  *
  * @c: the int number that will check
  * Returns 1 if c is a digit
- * Returns 0 otherwise
+ * 
 */
 
 int _isdigit(int c)
 {
-  if (c > '0' && c < 9)
+  if (c >= '0' && c =< 9)
     return(1);
   else
     return(0);
