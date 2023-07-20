195fb45a9398af26a06116ad003dd9e068f26174
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-20 14:21:04 +0300

    MOre function more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _isupper(int c) - function that checks for uppercase character.
- * @c: the int for the parameter of the function
- * 
- * Returns 0 otherwise
- *
+ * _isupper - function that checks for uppercase character.
+ * @c: the int for the paramaters of my function
+ * Return: 0
 */
-
 int _isupper(int c)
 {
-  if (c >= 65 && c <= 90)
-    {
-      return (1);
-    }
-  else
-    return (0);
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
 }
