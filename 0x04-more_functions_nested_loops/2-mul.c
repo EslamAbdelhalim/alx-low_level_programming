1bd217f595ec86f92277154a3e6705f12f00b4a8
EslamAbdelhalim <eslamelgarhy041@gmail.com>
2023-07-20 14:40:24 +0300

    MOre function more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers.
+ *
+ * @a: the first integer to multiplie
+ * @b: the second integer to multiplie
+ *
+ * return (a * b) the result of multiplication
+*/
+int mul(int a, int b)
+{
+  return (a * b);
+}
